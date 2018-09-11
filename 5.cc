b42a22e481734bfddabd843dbcc2bcc33fa883c2
SersBB <31618733+SersBB@users.noreply.github.com>
2018-09-11 09:47:06 +0200

    Create 5.cc

---
+++ 5.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  float a=0,b=0,c=0;
+  if((a+b<c||a+c<b||b+c<a)&&a>0&&b>0&&c>0){
+    cout<<"je mogoc";
+  }else{
+    cout<<"ni mogoc";
+  }
+  return 0;
+}
