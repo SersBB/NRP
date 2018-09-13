75be74f1af4c16fd05fb47dbd1d7ca50af534a49
SersBB <31618733+SersBB@users.noreply.github.com>
2018-09-13 10:54:18 +0200

    Create 6.cc

---
+++ 6.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string a;
+  cin>>a;
+  if(a=="ponedeljek"||a=="torek")
+    cout<<"Mateja"<<endl;
+  if(a=="sreda")
+    cout<<"Irena"<<endl;
+  if(a=="cetrtek")
+    cout<<"Tadeja"<<endl;
+  if(a=="petek")
+    cout<<"Jana"<<endl;
+  return 0;
+}
