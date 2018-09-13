f1e50a0f1ebb63866e5ab5dee988239f838501e9
SersBB <31618733+SersBB@users.noreply.github.com>
2018-09-13 11:24:11 +0200

    Create 9.cc

---
+++ 9.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int a;
+  cout<<"Vpisi St. mesca"<<endl;
+  cin>>a;
+  if(a==1){
+        cout<<"Jaka, Mojca, Spela Meseca"<<endl;
+  }else
+  if(a==2){
+        cout<<"Tjaša, Jasna, Luka"<<endl;
+  }else
+  if(a==3){
+        cout<<"Matej, Janez"<<endl;
+  }else
+  if(a==4){
+        cout<<"Tomi, Jožica, Ančka, Lojzka"<<endl;
+  }else
+  if(a==5){
+        cout<<"Jakob, Mojceja, Zlatka"<<endl;
+  }else
+  if(a==6){
+        cout<<"Zvonko, Simon, Simona"<<endl;
+  }else
+  if(a==7){
+        cout<<"Jakob, Mojceja, Zlatka"<<endl;
+  }else
+  if(a==8){
+        cout<<"Branka, Barbara, Žiga, Juljana, Tea"<<endl;
+  }else
+  if(a==9){
+        cout<<"Klemen, Katarina, Klavdija"<<endl;
+  }else
+  if(a==10){
+        cout<<"Primoz, Valentin, Maja, Zofi"<<endl;
+  }else
+  if(a==11){
+        cout<<"Žan, Zvezdana, Valentino"<<endl;
+  }else
+  if(a==12){
+        cout<<"Marko, Maruša, Janja, Shakira"<<endl;
+  }else
+        cout<<"Mesec je bil napacno vnesen";
+  return 0;
+}
