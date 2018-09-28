f74c1d2680441499b222c9bfa374561652bd72f6
SersBB <31618733+SersBB@users.noreply.github.com>
2018-09-28 10:15:43 +0200

    Create 04.cc

---
+++ 04.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int i=0,k,r=0;
+    cin>>k;
+    int vek[k][k]={0};
+    for(i=0;i<k;i++){
+        for(r=0;r<k;r++){
+                if(r<k-i)
+                     vek[i][r]=r+1;
+        else vek[i][r]=0;
+        }
+    }
+    for(i=0;i<k;i++){
+        for(r=0;r<k;r++){
+        cout<<vek[i][r]<<"  ";
+        }
+        cout<<endl;
+    }
+}
