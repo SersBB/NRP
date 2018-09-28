580e8183d3d0ebf8e89f1e00088480c70d6102de
SersBB <31618733+SersBB@users.noreply.github.com>
2018-09-28 10:08:15 +0200

    Create 03.cc

---
+++ 03.cc
@@ -0,0 +1,12 @@
+       #include <iostream>
+       using namespace std;
+       
+       int main()
+       {
+           int i=1,a=0;
+           while(i<=5){
+               a+=i;
+               i++;
+           }
+           cout<<a;
+       }
