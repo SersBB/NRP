6516a13c3087c81cd800dbe9f06152ed02d96fc7
SersBB <31618733+SersBB@users.noreply.github.com>
2018-09-28 10:08:46 +0200

    Create 04.cc

---
+++ 04.cc
@@ -0,0 +1,13 @@
+       #include <iostream>
+       using namespace std;
+       
+       int main()
+       {
+           int i=1,a=0,b;
+           while(i<=5){
+                   cin>>b;
+               a+=b;
+               i++;
+           }
+           cout<<a;
+       }
