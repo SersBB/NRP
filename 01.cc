d9d4b9e6bd8810bb6a43c0ff163444920031fff9
SersBB <31618733+SersBB@users.noreply.github.com>
2018-09-28 10:14:01 +0200

    Update and rename 1.cc to 01.cc

---
+++ 01.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int i=0,k,r=0;
+    cin>>k;
+    int vek[k][k]={0};
+    for(i=0;i<k;i++){
+        for(r=0;r<k;r++){
+                     vek[i][r]=i+1-r;
+                     if(vek[i][r]<0)vek[i][r]=0;
+        }
+    }
+    for(i=0;i<k;i++){
+        for(r=0;r<k;r++){
+        cout<<vek[i][r]<<"  ";
+        }
+        cout<<endl;
+    }
+
+}
