9604b18d0dc596924edef22be55a4b8b9081fa47
SersBB <31618733+SersBB@users.noreply.github.com>
2018-09-29 09:48:12 +0200

    Create 11.cc

---
+++ 11.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int k;
+    cin>>k;
+int input[k];
+for(int i =0; i<k;i++){
+    cin>>input[i];
+}
+for(int i =0; i<k;i++){
+    cout<<input[i]<<endl;
+}
+return 0;
+}
