f8a5af528c0566b7e25f730614db093e429d6c50
SersBB <31618733+SersBB@users.noreply.github.com>
2018-09-29 09:56:49 +0200

    Create 12.cc

---
+++ 12.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int k, v=0;
+    cin>>k;
+int input[k];
+for(int i =0; i<k;i++){
+    cin>>input[i];
+    v+= input[i];
+}
+cout <<"="<<v/k;
+return 0;
+}
