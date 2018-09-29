3e6af5cac74a7b8b3312cc12acd784dba9e56c3f
SersBB <31618733+SersBB@users.noreply.github.com>
2018-09-29 09:33:50 +0200

    Create 08.cc

---
+++ 08.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int k;
+    cin>>k;
+int input[k];
+for(int i =0; i<k;i++){
+    cin>>input[i];
+}
+for(int i =0; i<k;i++){
+    if(input[i]%2==0)
+    cout<<input[i]<<endl;
+}
+return 0;
+}
